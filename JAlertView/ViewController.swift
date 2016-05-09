e585e62fce6e0d493df928586819b8f58d77dc30
“jinxiansens” <hi@jinxiansen.com>
2016-05-09 12:57:20 +0800

    Initial Commit

---
+++ JAlertView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JAlertView
+//
+//  Created by 开发者 on 16/5/9.
+//  Copyright © 2016年 jinxiansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
